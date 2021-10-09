709397d580ba61f3cec9900ee4f70108b753c2eb
Alex Blundell <alex@alexblundell.com>
2021-10-09 09:33:01 +0100

    Test commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+
+    const [hello, setHello] = useState('hello');
+
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+            {hello}
         </a>
       </header>
     </div>
